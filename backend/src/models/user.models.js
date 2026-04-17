dac18c87796da27beaf6cee9d3fee06cab2e8f4c
Sunil kumar <sunilkumar3020@gmail.com>
2026-04-17 10:44:16 +0530

    user model created

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,3 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({}, { timestamps: true })
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+
+}, { timestamps: true })
+const User = mongoose.model("User", userSchema);
