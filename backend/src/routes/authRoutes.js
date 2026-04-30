abaa333ac8e56608bc8b2d971b2cbc6083276f38
Sunil kumar <sunilkumar3020@gmail.com>
2026-04-30 17:35:20 +0530

    create new route for emailCheck in authRouter

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register, login, logout } from "../controllers/authController.js"
+import { register, login, logout, emailCheck } from "../controllers/authController.js"
 const router = express.Router();
 
 router.post('/register', register);
+router.post("/emailCheck", emailCheck)
 router.post('/login', login);
 router.post('/logout', logout)
 
